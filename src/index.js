7d54b7e4edd3d2f7606032c3227715051bdefeaf
nasimqureshi <qureshi.nasim@yahoo.com>
2021-02-01 10:39:45 -0600

    second commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Greeting(){
-  return <h1>This is nasim and this is my first component</h1>
+//function Greeting(){
+ // return <h1>This is nasim and this is my first component</h1>
+//}
+
+const Greeting = () => {
+  return React.createElement('h1', {}, "Hello Nasim how are you I am ok");
 }
 
 
