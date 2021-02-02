8c8ea31f6b95f9ca1404f8cdfedc367d76e06c72
nasimqureshi <qureshi.nasim@yahoo.com>
2021-02-02 11:27:44 -0600

    fifth commit

--- src/index.js
+++ src/index.js
@@ -26,16 +26,43 @@ import ReactDOM from 'react-dom';
 //};
 //ReactDOM.render(<Greeting />, document.getElementById('root'));
 
-const Greeting = () => {
-  return(
-    <div>
-      <Person />
-      <Message />
-    </div>
-  );
-};
-const Person = () => <h2>Nasim Qureshi</h2>
-const Message = () => {
-  return <p>This is nasim's message</p>
+//const Greeting = () => {
+  //return(
+    //<div>
+      //<Person />
+      //<Message />
+    //</div>
+  //);
+//};
+//const Person = () => <h2>Nasim Qureshi</h2>
+//const Message = () => {
+  //return <p>This is nasim's message</p>
+//}
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+
+function BookList () {
+  return (
+  <section>
+    <Book />
+  </section>
+  )
 }
-ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+const Book = () => {
+return (
+  <article>
+  <Image />
+  <Title />
+  </article>
+  
+)
+}
+const Image =() => (
+<Image src="https://img.thriftbooks.com/api/images/s/9fbc5bad7fca23370dfda95285763876796aac20.jpg" alt="" /> 
+)
+const Title = () => (
+  <h1>Llama Llama I Love YouLlama Llama </h1>
+)
+  
+ReactDOM.render(<BookList />, document.getElementById('root'));
